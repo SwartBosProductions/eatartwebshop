f027723dced84f4160d52334a35606a9be9f9d33
JorritBosma <bosma.jorrit@gmail.com>
2021-02-12 11:58:20 +0100

    Collection migration/seed/model/controller.

---
+++ database/seeders/CollectionSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CollectionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('collections')->insert([
+            [
+                'collection_name' => 'EatArt Christmas 2020',
+                'created_by' => 1,
+            ],
+            [
+                'collection_name' => 'EatArt Autumn 2021',
+                'created_by' => 1,
+            ],
+            [
+                'collection_name' => 'EatArt Valentine 2022',
+                'created_by' => 1,
+            ],
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             UsergroupSeeder::class,
             UserSeeder::class,
             DesignerSeeder::class,
+            CollectionSeeder::class,
         ]);
     }
 }

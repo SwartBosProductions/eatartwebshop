4389464a984ee2a9cb83a83c40cf9dcecbb462a1
FW-Swart <folkert@swartwerk.nl>
2021-02-20 07:37:46 +0100

    puiblic views clean up / styling

--- resources/views/shop/product_detail.blade.php
+++ resources/views/shop/product_detail.blade.php
@@ -8,7 +8,9 @@
             <div class=" col-12 col-lg-10 bg-light">
                 <div class="row">
                     <div class="col-12 bg-light">
-                        <h2>Serie product detail pagina</h2>
+                        <h2>
+                            Serie: naam van selected
+                        </h2>
                     </div>
                 </div>
             </div>
@@ -18,30 +20,39 @@
     <div class="main-shop-content container-fluid p-2 my-2 mx-auto">
         <div class="row justify-content-center">
             <div class=" col-10 col-lg-8 bg-light text-center">
-                <article>
-                    <h3>
-                        Product 1
-                    </h3>
-                    <img src="images/is-dit-kunst-of-kan-het-weg.png" alt="">
-                </article>
-                <article>
-                    <h3>
-                        Product 2
-                    </h3>
-                    <img src="images/is-dit-kunst-of-kan-het-weg.png" alt="">
-                </article>
-                <article>
-                    <h3>
-                        Product 3
-                    </h3>
-                    <img src="images/is-dit-kunst-of-kan-het-weg.png" alt="">
-                </article>
-                <article>
-                    <h3>
-                        Product 4
-                    </h3>
-                    <img src="images/is-dit-kunst-of-kan-het-weg.png" alt="">
-                </article>
+
+                <div class="row p-2">
+
+                    {{-- @foreach product in serie current id --}}
+                    <div class="col-3 p-2 m-2">
+                        <section class="">
+
+                                {{-- = link naar product detail page / daar knop koop en gaat ie in de winkelmand --}}
+                                {{-- <img src="product picture" alt="voorbeeld afbeelding van serie product" height="80"> --}}
+                                {{-- * blur if sold --}}
+                                <h3>
+                                    picture
+                                    product naam
+                                </h3>
+
+                            <div class="">
+                                <h4>
+                                    * description
+                                </h4>
+                                <h4>
+                                    * designer name
+                                    * if ditale link foreach
+                                </h4>
+                                <h4>
+                                    * Wat willen we hier nog meer vertellen?
+                                </h4>
+                            </div>
+                        </section>
+                    </div>
+                    {{-- @endforeach --}}
+
+                </div>
+
             </div>
         </div>
     </div>

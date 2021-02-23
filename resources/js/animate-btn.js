90da82fc2ab2a8e52463856589ed4031b6a696fe
FW-Swart <folkert@swartwerk.nl>
2021-02-23 21:38:46 +0100

    web shop content sauce eerst op product.detaile .blade

--- resources/js/animate-btn.js
+++ resources/js/animate-btn.js
@@ -2,7 +2,7 @@
 
 // public pages inlog en register button on header animation
 // nog afmaken voor de leuk een keer
-const btn = document.querySelector('.btn-inlog-register');
+const btn = document.querySelector('.btn-product-buy');
 
 btn.addEventListener('click', () => {
   btn.classList.add('anim');

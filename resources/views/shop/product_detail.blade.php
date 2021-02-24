f28f45b5345288b27c29d321373eacc030582ab6
FW-Swart <folkert@swartwerk.nl>
2021-02-24 09:00:56 +0100

    minor styling changes

--- resources/views/shop/product_detail.blade.php
+++ resources/views/shop/product_detail.blade.php
@@ -11,10 +11,12 @@
                 <div class="row p-2 serie-detail-content">
                     <div class="col-6">
                         <h2 class="h2-product-serie">
-                            Serie: {{$serie_name}}
+                            <strong>Serie :</strong>
+                            {{$serie_name}}
                         </h2>
                         <h3 class="h3-product-designer">
-                            Designer: {{$designer}}
+                            <strong>Designer :</strong>
+                            {{$designer}}
                         </h3>
                     </div>
                     <div class="col-6">
@@ -32,23 +34,24 @@
 
         <div class="row justify-content-center my-1">
 
-            <div class=" col-12 col-lg-8 text-center">
+            <div class=" col-10 col-lg-8 text-center">
 
                 <div class="row p-2">
-                    <div class="col-12 p-2 product-detail-content">
+                    <div class="col-12 px-2 pt-0 pb-2 product-detail-content">
                         @foreach ($products as $product)
                         <div class="row my-2 product-scroll-center" data-spy="scroll">
 
                             <div class="col-4">
                                 <div class="row">
+                                    <h3 class="h3-product-detail py-2 px-3 my-2 justify-content-arround">
+
                                     {{-- click picture enlarge in centerd frame --}}
                                     <img src="{{$product->picture}}" alt="afbeelding van het product"
-                                        class=" m-0 p-2 img-inlist-product">
-                                </div>
-                                <div class="row justify-content-center my-auto">
-                                    <h3 class="h3-product-detail py-2 px-3 my-2 justify-content-arround">
+                                        class=" mx-4 p-2 img-inlist-product">
                                         {{$product->product_name}}
+
                                     </h3>
+
                                 </div>
                             </div>
 
@@ -65,13 +68,22 @@
                             </div>
 
                             <div class="col-2 py-4">
-                                <div class="row p-1 justify-content-center product-price">
+                                <div class="row p-1 product-price my-auto">
                                     <h4 class="h4-product-price p-2">
                                         {{-- nice to have old fashion price stamp gun frame in fluor orange --}}
                                         &euro; {{$product->price}}
                                     </h4>
                                 </div>
-                                <div class="row p-1 justify-content-center">
+                                <div class="row p-0 m-0 justify-content-end">
+                                    <sup class=""">
+                                        excl. BTW
+                                    </sup>
+                                </div>
+
+
+
+
+                                <div class="row px-1 py-4 justify-content-center">
                                     <div class="">
                                         <a href="{{ route('addToCart', ['id' => $product->id]) }}"
                                             class="btn btn-inlog-register eatart-a1 product-buy">

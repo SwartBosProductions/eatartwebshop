9f1044e2875665f6098a37c21844fd31c40df579
FW-Swart <folkert@swartwerk.nl>
2021-02-26 06:54:42 +0100

    last commit for demoday

--- resources/views/components/shop/order-button.blade.php
+++ resources/views/components/shop/order-button.blade.php
@@ -1,4 +1,4 @@
 {{-- if shopping cart =0 disable --}}
-    <div {{ $attributes->merge(['class' => 'btn main-home-button my-auto', 'type' => 'button']) }}>
+    <div {{ $attributes->merge(['class' => 'btn shop-order-btn my-auto', 'type' => 'button']) }}>
         Bestellen
     </div>
